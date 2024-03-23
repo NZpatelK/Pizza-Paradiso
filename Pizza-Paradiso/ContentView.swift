2533527c72f182709e6835d0f1e69cc7fa1f68f1
NZpatelK <patelk.dev@hotmail.com>
2024-03-23 17:00:30 +1300

    Update ContentView.swift

--- Pizza-Paradiso/ContentView.swift
+++ Pizza-Paradiso/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     
-    private let pizza = ["pizza-1", "pizza-2", "pizza-3", "pizza-4"]
+    private let pizzaImgs = ["pizza-1", "pizza-2", "pizza-3", "pizza-4"]
     var body: some View {
-        VStack {
+        ZStack {
             Image("pan")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 300, height: 300)
+            ScrollView(.horizontal) {
+                HStack{
+                    ForEach (pizzaImgs, id: \.self) { pizza in
+                        Image(pizza)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 280, height: 280)
+                            .containerRelativeFrame(.horizontal, count: 1, spacing: 4)
+                    }
+                }
+            }
         }
         .padding()
     }
