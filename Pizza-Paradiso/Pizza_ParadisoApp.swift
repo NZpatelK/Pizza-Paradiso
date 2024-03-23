0e5ed0f28d272f5a544b43de4cc6738ef9500d10
NZpatelK <patelk.dev@hotmail.com>
2024-03-23 15:55:04 +1300

    Initial Commit

---
+++ Pizza-Paradiso/Pizza_ParadisoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_ParadisoApp.swift
+//  Pizza-Paradiso
+//
+//  Created by Karan Patel on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_ParadisoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
