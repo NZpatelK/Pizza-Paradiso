282b68901a2db7083b131e1396560fb3c9a70550
NZpatelK <patelk.dev@hotmail.com>
2024-03-23 16:40:34 +1300

    upload image and display it

--- Pizza-Paradiso/ContentView.swift
+++ Pizza-Paradiso/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let pizza = ["pizza-1", "pizza-2", "pizza-3", "pizza-4"]
     var body: some View {
         VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
+            Image("pan")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
         }
         .padding()
     }
