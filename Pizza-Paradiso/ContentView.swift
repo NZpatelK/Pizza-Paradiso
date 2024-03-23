aae601dfb80dc49925c087598f6e88b6fc9c87d5
NZpatelK <patelk.dev@hotmail.com>
2024-03-23 18:33:53 +1300

    add the function of the rotation pizza

--- Pizza-Paradiso/ContentView.swift
+++ Pizza-Paradiso/ContentView.swift
@@ -44,44 +44,17 @@ import SwiftUI
 
 
 struct ContentView: View {
-    private let pizzaImgs = ["pizza-1", "pizza-2", "pizza-3", "pizza-4"]
-    @State private var scrollOffset: CGFloat = 0
-    
+   
     var body: some View {
-        ZStack {
-            Image("pan")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 250, height: 250)
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 4) {
-                    ForEach(pizzaImgs.indices, id: \.self) { index in
-                        GeometryReader { geo in
-                            Image(pizzaImgs[index])
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 240, height: 240)
-                                .rotationEffect(Angle(degrees: Double((geo.frame(in: .global).midX - scrollOffset) / UIScreen.main.bounds.width) * 90))
+        NavigationView {
+                    List {
+                        NavigationLink(destination: PizzaRotateSlide()) {
+                            Text("Pizza Rotation Slide")
                         }
-                        .frame(width: 240, height: 240)
                     }
-                    .containerRelativeFrame(.horizontal, count: 1, spacing: 4)
+                    .navigationTitle("Main Menu")
                 }
-                .frame(height: 280)
-                .onAppear {
-                    DispatchQueue.main.async {
-                        scrollOffset = UIScreen.main.bounds.width * CGFloat(pizzaImgs.count)
-                    }
-                }
-                .onChange(of: scrollOffset) { _ in
-                    if scrollOffset > UIScreen.main.bounds.width * CGFloat(pizzaImgs.count - 1) {
-                        scrollOffset = UIScreen.main.bounds.width * CGFloat(pizzaImgs.count - 1)
-                    }
-                }
-                .scrollTargetLayout()
-            }
-            .scrollTargetBehavior(.viewAligned)
-        }
+
     }
 }
 
