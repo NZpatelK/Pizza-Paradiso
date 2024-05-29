3d8911695bb795497689c0b2651b0b3ef432a100
NZpatelK <patelk.dev@hotmail.com>
2024-05-29 12:54:11 +1200

    Update PizzaApp.swift

--- Pizza-Paradiso/Pages/PizzaApp.swift
+++ Pizza-Paradiso/Pages/PizzaApp.swift
@@ -8,8 +8,105 @@
 import SwiftUI
 
 struct PizzaApp: View {
+    @State private var pizzaSize: CGFloat = 280
+    @State private var panSize: CGFloat = 300
+    @State private var Selected:String = "S"
+    @State private var isSelected:Bool = false
+    
+    private let pizzaImgs = ["pizza-1", "pizza-2", "pizza-3", "pizza-4"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{ geometry in
+            VStack(){
+                ZStack {
+                    Image("pan")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 300)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack{
+                            ForEach (pizzaImgs, id: \.self) { pizza in
+                                Image(pizza)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 280, height: 280)
+                                    .containerRelativeFrame(.horizontal, count: 1, spacing: 4)
+                                    .scrollTransition{content, phase in
+                                        content
+                                            .opacity(phase.isIdentity ? 1.0 : 0)
+                                            .scaleEffect(x: phase.isIdentity ? 1 : 0.3,
+                                                         y: phase.isIdentity ? 1 : 0.3)
+                                            .offset(y: phase.isIdentity ? 0 : 50)
+                                    }
+                            }
+                        }
+                        .scrollTargetLayout()
+                    }
+                    .scrollTargetBehavior(.viewAligned)
+                }
+            }
+            .position(x: geometry.size.width / 2, y: 150)
+            
+            Spacer()
+            HStack(spacing: 20) {
+                ZStack{
+                    Button(action:  {
+                        panSize = 300
+                        pizzaSize = 280
+                        Selected = "S"
+                        isSelected = true
+                    })
+                    {
+                        Capsule()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(Selected == "S" ? Color.orange : Color.gray)
+                            .overlay(
+                                Text("S")
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundColor(Color.white)
+                            )
+                    }
+                }
+                ZStack{
+                    Button(action:  {
+                        panSize = 350
+                        pizzaSize = 330
+                        Selected = "M"
+                        isSelected = true
+                    })
+                    {
+                        Capsule()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(Selected == "M" ? Color.orange : Color.gray)
+                            .overlay(
+                                Text("M")
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundColor(Color.white)
+                            )
+                    }
+                }
+                ZStack{
+                    Button(action:  {
+                        panSize = 400
+                        pizzaSize = 380
+                        Selected = "L"
+                        isSelected = true
+                    })
+                    {
+                        Capsule()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(Selected == "L" ? Color.orange : Color.gray)
+                            .overlay(
+                                Text("L")
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundColor(Color.white)
+                            )
+                    }
+                }
+            }
+            .position(x: geometry.size.width / 2, y: geometry.size.height-200)
+            Spacer()
+        }
     }
 }
 
